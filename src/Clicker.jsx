1d16bfc08c2ffbf21a2874e8daa98afdc058aa63
David Ricketts <david.ricketts119@gmail.com>
2023-09-07 11:46:46 -0400

    Clicker component

---
+++ src/Clicker.jsx
@@ -0,0 +1,21 @@
+function handleClick() {
+    console.log("Clicked the button!!!");
+}
+
+function handleHover() {
+    console.log("HOVERED!!");
+}
+
+export default function Clicker() {
+    return (
+        <div>
+            <p onMouseOver={handleHover}>Hover Over Me</p>
+            <button onClick={handleClick}>Click</button>
+            <button
+                onClick={() =>
+                    console.log("CLICKED THE INLINE ARROW FUNCTION VERSION")}>
+                        Inline Function (CLICK ME!)
+                    </button>
+        </div>
+    )
+}
