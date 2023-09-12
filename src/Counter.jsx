4c0f62e99a3ed96d781260c1b30bb61c8e3c6309
David Ricketts <david.ricketts119@gmail.com>
2023-09-12 06:57:44 -0400

    Clicker component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 
 export default function Counter() {
-    const [num, setNum] = useState(5);
-    console.log("COMPONENT HAS BEEN EXECUTED AGAIN!");
+    console.log("RENDERED!!!");
+    const [count, setCount] = useState(0);
     console.log(`num: ${num}`);
-    const changNum = () => {
-        setNum(num + 1);
-        console.log("SET NUM HAS RUN!");
-        console.log(`num: ${num}`);
+    const addOne = () => {
+        setCount(count + 1);
+    };
+    const setToTen = () => {
+        setCount(10);
+    };
+    const addThree = () => {
+        setCount((currentCount) => currentCount + 1);
+        console.log(count);
+        setCount((currentCount) => currentCount + 1);
+        setCount((currentCount) => currentCount + 1);
     };
     return (
         <div>
-            <p>The count is {num}</p>
-            <button onClick={changNum}>Increment</button>
+            <p>The count is {count}</p>
+            <button onClick={addOne}>+1</button>
+            <button onClick={addThree}>+3</button>
+            <button onClick={setToTen}>Set To 10</button>
         </div>
 
     )
